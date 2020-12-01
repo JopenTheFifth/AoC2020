4a588ca6e8755b0f4f5ccfebe6ab587681386487
Joël <0252622@student.rocvantwente.nl>
2020-12-01 18:15:31 +0100

    Revert "f"
    This reverts commit 0bdf8f64f3f58373f33851fbc5b884c2d5f4b315.

---
+++ AoC_Day_1/AoC_Day_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace AoC_D1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamReader myfile = new StreamReader(@"C:\Users\joelr\source\repos\\AoC_D1\AoC_D1\data\numbers.txt");
+            var numbers = myfile.ReadToEnd().Split('\n').Select(Int32.Parse).ToList();
+            int total = 0;
+            foreach (var t in numbers)
+            {
+                foreach (var t1 in numbers)
+                {
+                    foreach (var t2 in numbers)
+                    {
+                        if (t + t1 + t2 != 2020) continue;
+                        total = 0;
+                        total = (t * t1 * t2);
+                    }
+                }
+            }
+        }
+    }
+}
