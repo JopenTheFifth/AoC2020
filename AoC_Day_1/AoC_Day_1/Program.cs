e2de6c577c7c81fb343e35f5bd71d8e38cee6f73
Joël <55190697+Jopenthe5TH@users.noreply.github.com>
2020-12-02 10:54:20 +0100

    readded day 1
    readded day 1

---
+++ AoC_Day_1/AoC_Day_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myfile = new StreamReader(@"C:\Users\joelr\source\repos\\AoC_D1\AoC_D1\data\numbers.txt");
+            var numbers = myfile.ReadToEnd().Split('\n').Select(Int32.Parse).ToList();
+            int total = 0;
+            foreach (var t in numbers)
+            {
+                foreach (var t1 in numbers)
+                {
+                    foreach (var t2 in numbers)
+                    {
+                        if (t + t1 + t2 != 2020) continue;
+                        total = 0;
+                        total = (t * t1 * t2);
+                    }
+                }
+            }
+        }
+    }
+}
