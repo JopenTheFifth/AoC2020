7434d0feac06e86e7dd6cb186a68a17f5ff886bc
Joël <0252622@student.rocvantwente.nl>
2020-12-03 13:25:45 +0100

    completion day3
    completion day3

---
+++ AoC_Day_3/AoC_Day_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"C:\Users\joelr\AoC2020\AoC_Day_3\AoC_Day_3\inputs.txt");
+            var width = lines[0].Length; //31
+            var height = lines.Length; // 323
+
+            char[,] map = new char[height, width];
+            
+            for (int i = 0; i < height; i++)
+            {
+                var data = lines[i];
+
+
+                for (int j = 0; j < width; j++)
+                {
+                    map[i, j] = data[j];
+                }
+            }
+
+            //214, 94, 99, 91, 46
+
+            var widthPos = 0;
+            var heightPos = 0;
+            var counter1 = 0;
+            for (int i = 0; i < height; i++)
+            {
+
+                if (widthPos > 30)
+                {
+                    widthPos -= 31;
+                }
+
+                if (map[heightPos, widthPos] == '#')
+                {
+                    counter1 += 1;
+                }
+
+                widthPos += 3;
+                heightPos += 1;
+
+            }
+            Console.WriteLine(counter1);
+            Console.ReadKey();
+
+        }
+    }
+}
