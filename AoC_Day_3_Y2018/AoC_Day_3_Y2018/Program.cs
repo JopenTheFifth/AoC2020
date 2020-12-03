81f1a0938108a684ba1602b58ca499776fdfe8f6
Joël <0252622@student.rocvantwente.nl>
2020-12-03 11:02:18 +0100

    f
    f

--- AoC_Day_3_Y2018/AoC_Day_3_Y2018/Program.cs
+++ AoC_Day_3_Y2018/AoC_Day_3_Y2018/Program.cs
@@ -23,7 +23,7 @@ namespace AoC_Day_3_Y2018
             {
                 var parsed = onBreak[i].Split(' ');
                 var claimId = parsed[0];
-                var margin = parsed[2].Split(',');
+                var margin = parsed[2].Trim(':').Split(',');
                 var dimensions = parsed[3].Split('x');
 
 
@@ -35,37 +35,33 @@ namespace AoC_Day_3_Y2018
                 var height = dimensions[1];
 
 
+                collection[startFromLeft, startFromTop] = $"{claimId}";
 
-
-                Buffer.BlockCopy(collection, startFromLeft, startFromTop )
-                //encapsulate a part of the 2d array based on the start and end position
-                // if its 10 inches from the left, and 20 in width, we can calculate the end point
-                // in this case, 10 + 20 = 30.
-
-
-
-                
-               
             }
 
-            //for (int i = 0; i < collection.GetLength(0); i++)
-            //{
-            //    Console.WriteLine(collection[i, 0] + " " + collection[i, 1]);
-            //}
+
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                for (int j = 0; j < collection.Length; j +=2)
+                {
+                    Console.WriteLine($"{collection[i, j]}, {collection[i, j]}");
+                }
+            }
 
             Console.ReadLine();
         }
 
 
 
-        public static char[,] CreateArray()
+        public static string[,] CreateArray()
         {
-            char[,] array = new char[1000, 1000];
+            string[,] array = new string[1000, 1000];
             for (int i = 0; i < 1000 -1; i++)
             {
                 for (int j = 0; j < 1000; j++)
                 {
-                    array[i, j] = '.';
+                    array[i, j] = ".";
                 }
             }
             return array;
